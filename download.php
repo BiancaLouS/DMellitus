8744bb3c1dba03f1b31e7c625315610a4dedef33
BIANCA LOURENCO <bianca.lourenco@senacsp.edu.br>
2023-04-27 17:22:53 -0300

    Tentativa de manutenção carousel

--- download.php
+++ download.php
@@ -10,46 +10,7 @@
 <!-- cabeçario -->
 
 <!-- botao download -->
-  <main>
-    <div>
-        
-      <div class="container-fluid text-center">
-        <div class="row m-4">
-          <div class="col text-black mt-5">
-            <h3>
-             Faça seu Download
-            </h3>
-            <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-              <button type="button" class="btn btn-primary">Download</button>            
-              <div class="btn-group" role="group">
-                <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                </button>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Android</a></li>
-                  <li><a class="dropdown-item" href="#">Iphone</a></li>
-                </ul>
-              </div>
-            </div>
-          <div class="d-flex justify-content-center">
-            <h5 class="text-primary">Android</h5>&nbsp;&&nbsp; <h5 class="text-primary">Iphone</h5>
-          </div>
-          </div>
-          <div class="col">
-            <div id="carouselExampleSlidesOnly" class="carousel slide border border-2 rounded-2" data-bs-ride="carousel">
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="img/carrosselmeio.png" class="d-block w-100 my-4 rounded-2" alt="...">
-                </div>
-                <div class="carousel-item">
-                  <img src="img/carrocel.png" class="d-block w-100 my-4 rounded-2" alt="...">
-                </div>
-                <div class="carousel-item">
-                  <img src="img/carrocel.png" class="d-block w-100 my-4 rounded-2" alt="...">
-                </div>
-              </div>
-            </div>
-          </div>
-        <div>
+  
     
   </main>
 </body>
