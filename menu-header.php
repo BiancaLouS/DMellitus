6bd045e90b1a19328844d50eabba8bcb9c0133a5
BIANCA LOURENCO <bianca.lourenco@senacsp.edu.br>
2023-04-27 16:22:52 -0300

    organizando códigos do menu-header.php

--- menu-header.php
+++ menu-header.php
@@ -32,46 +32,55 @@
                 <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Mais Opções</h5>
                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
-            <div class="offcanvas-body">
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Entre em Contato</a>
-                        <hr>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> Curiosidades </a>
-                        <hr>
-                    </li>
-                    <li class="nav-item dropdown " >
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Mais Sobre o Aplicativo
-                        </a>
-
-                        <ul class="dropdown-menu dropdown-menu-dark">
-                            
-                        <li><a class="dropdown-item" href="#">Administração de Glicemia</a></li>
-                        <li><a class="dropdown-item" href="#">Exercicíos e Receitas</a></li>
-                        <li><a class="dropdown-item" href="#">Outras Ferramentas</a></li>
-
-
-
-                        <li>
-                            <hr class="dropdown-divider">
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Entre em Contato</a>
+                            <hr>
                         </li>
-                        <li><a class="dropdown-item" href="#">O Cadastro   </a></li>
-                        <li><a class="dropdown-item" href="#">Políticas e Termos de Uso</a></li>
-                        <li><a class="dropdown-item" href="#">Para Menores de Idade</a></li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"> Curiosidades </a>
+                            <hr>
+                        </li>
 
-                        
-                        </ul>
-                        <hr>
-                </li>
-                </ul>
-                <form class="d-flex mt-3" role="search">
-                <input class="form-control me-2" type="search" placeholder="O que está procurando?" aria-label="Search">
-                <button class="btn btn-danger" type="submit">Pesquisar</button>
-                </form>
-            </div>
+                        <!-- Dropdown para maisn informações do aplicativo  -->
+
+                        <li class="nav-item dropdown " >
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Mais Sobre o Aplicativo
+                            </a>
+
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                    
+                                <li><a class="dropdown-item" href="#">Administração de Glicemia</a></li>
+
+                                <li><a class="dropdown-item" href="#">Exercicíos e Receitas</a></li>
+
+                                <li><a class="dropdown-item" href="#">Outras Ferramentas</a></li>
+
+                                <li> <hr class="dropdown-divider"> </li>
+
+                                <li><a class="dropdown-item" href="#">O Cadastro   </a></li>
+
+                                <li><a class="dropdown-item" href="#">Políticas e Termos de Uso</a></li>
+
+                                <li><a class="dropdown-item" href="#">Para Menores de Idade</a></li>
+
+                            
+                            </ul>
+
+                            <hr>
+                    </li>
+                    </ul>
+
+                    <!-- Barra de pesquisa  -->
+
+                    <form class="d-flex mt-3" role="search">
+                        <input class="form-control me-2" type="search" placeholder="O que está procurando?" aria-label="Search">
+                        <button class="btn btn-danger" type="submit">Pesquisar</button>
+                    </form>
+
+                </div>
             </div>
         </div>
     </nav>
