6fda8989dfc6fe7911e79e94305fc4f9a6c0b353
BIANCA LOURENCO <bianca.lourenco@senacsp.edu.br>
2023-04-27 16:49:02 -0300

    Adicionando imagens atualizadas do carrossel e tentativa de manutenção de carossel do menu

--- carousel.php
+++ carousel.php
@@ -9,6 +9,19 @@
 </head>
 <body>
   
- 
+  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">  
+      <div class="carousel-item active">
+        <img src="Img/bannerDiabetes.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="Img/bannerDiabetes.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="Img/bannerDiabetes.png" class="d-block w-100" alt="...">
+      </div>
+    </div>
+  </div>
+
 </body>
 </html>
