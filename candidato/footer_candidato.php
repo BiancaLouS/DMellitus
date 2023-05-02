b95406689d834b5e4e2447f446ac8488af3d40de
guisantosdesenvolvedor <guikerme.1707@gmail.com>
2023-05-02 17:18:34 -0300

    fazendo a parte de candidato (inicio) colocando o grid.

---
+++ candidato/footer_candidato.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css"  type="text/css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="font-awesome-4.4.0/css/font-awesome.min.css"  type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Asap:400,700' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+<footer>
+		<div class="wrap-footer">
+			<div class="container">
+				<div class="row"> 
+					<div class="col-footer col-md-3">
+						<h2 class="footer-title">SOBRE NÓS</h2>
+						<div class="textwidget">Aenean feugiat in ante et blandit. Vestibulum posuere molestie risus, ac interdum magna porta non. Pellentesque rutrum fringilla elementum. Curabitur tincidunt porta lorem vitae accumsan. <br> <br> 
+						Aenean feugiat in ante et blandit. Vestibulum posuere molestie risus, ac interdum magna porta non. Pellentesque rutrum fringilla elementum. Curabitur tincidunt porta lorem vitae accumsan.</div>
+					</div> 
+					<div class="col-footer col-md-3 widget_recent_entries">
+						<h2 class="footer-title">MAPA DO SITE</h2>
+						<ul>
+							<li><a href="#">HOME</a></li>
+							<li><a href="receita.php">RECEITAS</a></li>
+							<li><a href="#">CURIOSIDADES</a></li>
+							<li><a href="#">SOBRE O APP</a></li>
+						</ul>
+					</div>
+					<div class="col-footer col-md-3">
+						<h2 class="footer-title">ONG'S? </h2>
+						Bla-bla-bla<br><br>
+						<div class="textwidget">Rua Taquarana - Tatuapé, 2026</div>
+						<div class="textwidget">0123-456-789 E-mail: info@ognDiabetes.org</div>
+					</div>
+				
+
+                <!-- Enviar E-mail para empresa. -->
+
+                        <div class="col-footer col-md-3">
+                             <h2 class="footer-title">ENTRE EM CONTATO</h2>
+                             <br>
+                            <input type="email" placeholder="E-mail" class="form-control" id="Email">
+                            
+                        
+
+                            <select class="form-select" style="margin-top: 10px;">
+                                <option selected>Assunto</option>
+                                <option value="1">Informações</option>
+                                <option value="2">Duvidas</option>
+                                <option value="3">Suguestões</option>
+                                <option value="4">Suguestões</option>
+                                <option value="5">Elogios</option>
+                             </select>
+
+                             <div class="form-floating ">
+                                <textarea class="form-control" placeholder="Leave a comment here" style="margin-top: 10px;" id="floatingTextarea2" style="height: 100px"></textarea>
+                                <label for="floatingTextarea2">O que está pensando?</label>
+                            </div>
+
+                             <button type="submit" class="btn btn-danger" style="margin-top: 10px;">Enviar Mensagem!</button>
+                        </div>
+               
+
+                   
+                    
+				</div>
+			</div>
+		</div>
+		<div class="bottom-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<p>Copyright 2023 - Desenvolvida por Equipe Acessa DMellitus</a></p>
+					</div>
+					<div class="col-md-4">
+						<ul class="list-inline social-buttons">
+
+							<li>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+							</li>
+
+							<li>
+                                <a href=""><i class="fa fa-facebook"></i></a>
+							</li>
+
+							<li>
+                                <a href="#"><i class="fa fa-instagram"></i></a>
+							</li>
+						
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+    
+</body>
+</html>
