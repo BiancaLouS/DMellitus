9e7025c4a5b77b2a5e0483b464aedc41dc4d3794
guisantosdesenvolvedor <guikerme.1707@gmail.com>
2023-05-04 14:40:21 -0300

    comecando a fazer login

--- login/login.php
+++ login/login.php
@@ -1,27 +1,47 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/login.css">
-    <title>Document</title>
+    <title>Formulário</title>
 </head>
-<body>
-    <section class="container">
-        <div class="login">
-            <div class="logo">
 
-            </div>
-            <div class="form-login">
-                <form action="">
-                    <input type="email" name="email" id="" placeholder="Digite seu email" required>
-                    <input type="password" name="senha" id="" placeholder="Digite sua senha" required>
-                    <input type="button" value="entrar">
-                </form>
-                <p>Ainda não tem conta? <a href="">Criar conta</a></p>
-            </div>
+<body>
+    <div class="container">
+            <!-- <div class="form-image">
+                    <img src="img/logotst.webp" alt="">
+                </div> -->
+        <div class="form">
+            <form action="#">
+                <div class="form-header">
+                    <div class="title">
+                        <h1>Login</h1>
+                    </div>
+                    <div class="login-button">
+                        <button><a href="#">cadastrar-se</a></button>
+                    </div>
+                </div>
+
+                <div class="input-group">
+                    <div class="input-box">
+                        <label for="firstname">Primeiro Nome</label>
+                        <input id="firstname" type="text" name="firstname" placeholder="Digite seu primeiro nome" required>
+                    </div>
+
+                    <div class="input-box">
+                        <label for="lastname">Sobrenome</label>
+                        <input id="lastname" type="text" name="lastname" placeholder="Digite seu sobrenome" required>
+                    </div>
+                </div>  
+                <div class="continue-button">
+                    <button><a href="#">Finalizar</a> </button>
+                </div>
+            </form>
         </div>
-    </section>
+    </div>
 </body>
+
 </html>
