91cd610fd9e4f758e0448749f0ec5c40856c2139
guiguivida183882 <guikerme.1707@gmail.com>
2023-05-04 12:12:22 -0300

    comecando fazer area de login (login.php/login.css)

---
+++ login/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="container">
+        <div class="login">
+            <div class="logo">
+
+            </div>
+            <div class="form-login">
+                <form action="">
+                    <input type="email" name="email" id="" placeholder="Digite seu email" required>
+                    <input type="password" name="senha" id="" placeholder="Digite sua senha" required>
+                    <input type="button" value="entrar">
+                </form>
+                <p>Ainda não tem conta? <a href="">Criar conta</a></p>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
